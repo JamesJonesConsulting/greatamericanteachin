71112515396326b356c53b9c2e6e50ce0cc5fac1
James Jones <jamjon3@gmail.com>
2015-11-17 15:29:02 -0500

    A little more pre-work

--- app/scripts/controllers/gati.js
+++ app/scripts/controllers/gati.js
@@ -2,7 +2,7 @@
     'use strict';
     angular
     .module('gatiApp')
-    .controller('saqCtrl', ['$scope','$window','$location','$routeParams','$q', function ($scope,$window,$location,$routeParams,$q) { 
+    .controller('gatiCtrl', ['$scope','$window','$location','$routeParams','$q', function ($scope,$window,$location,$routeParams,$q) { 
             
     }]);
     
